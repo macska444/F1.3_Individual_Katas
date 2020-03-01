971685813db16b086ee5f0508642fc88b24e02b1
macska <macska444@gmail.com>
2020-03-01 11:02:34 +0100

    initial commit (1 failing test)

---
+++ magyar21_3_macska/src/main/java/magyar21/Hand.java
@@ -0,0 +1,9 @@
+package magyar21;
+
+import java.util.List;
+
+public class Hand {
+    public Integer evaluateHand(List<Integer> hand) {
+        return 0;
+    }
+}
---
+++ magyar21_3_macska/src/test/java/magyar21/HandTest.java
@@ -0,0 +1,20 @@
+package magyar21;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class HandTest {
+    protected Hand hand;
+
+    @Before
+    public void setUp() throws Exception {
+        hand = new Hand();
+    }
+
+    @Test
+    public void evaluateHand() {
+        assertFalse(true);
+    }
+}
